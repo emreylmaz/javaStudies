0f921db1bc2a24d41ea2eacf53b1a998e9469a2a
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-07-14 16:41:32 +0300

    Taximeter add.

---
+++ taxiMeter/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Değişkenlerin tanımlanması
+        double taxiMetre, acilis, ucret, yol ;
+
+        taxiMetre = 2.20;
+        acilis = 10.0;
+        ucret = 0.0;
+        yol = 0.0;
+
+        // Kullanıcıdan alınan değerler alınıyor
+        System.out.print("Lütfen mesafeyi giriniz: ");
+        Scanner scanner = new Scanner(System.in);
+
+        yol = scanner.nextDouble();
+
+        // Hesaplama işlemleri
+        ucret = acilis + (yol * taxiMetre);
+
+        // Ücret için en düşük değer şartı 20 TL'ye eşitleniyor.
+        if (ucret <= 20.0){
+            ucret = 20;
+        }
+
+        // Sonuçları ekrana yazdırılıyor
+        System.out.println("Ucretiniz: " + ucret);
+
+
+
+    }
+}
